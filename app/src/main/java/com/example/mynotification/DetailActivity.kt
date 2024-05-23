efdfd24817204b0ccfcf9bbd159f3b54c991f66c
prpleehyyth <ipulmixed@gmail.com>
2024-05-23 09:38:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mynotification/DetailActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mynotification
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mynotification.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        /*
+        Ambil data dari intent yang dikirimkan oleh notifikasi
+        */
+        val title = intent.getStringExtra(EXTRA_TITLE)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        binding.tvTitle.text = title
+        binding.tvMessage.text = message
+    }
+    companion object {
+        const val EXTRA_TITLE = "extra_title"
+        const val EXTRA_MESSAGE = "extra_message"
+    }
+}
